13c6eabf310401ab47d9a2f8f43344b668b11e0c
Suresh_Gobi <sureshgobi34@gmail.com>
2023-08-28 17:08:54 +0530

    Header and Redux

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/header";
+import Login from "./pages/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <Header />
+        <Routes>
+        <Route path="/" element={<Login/>} />
+        </Routes>
+      </Router>
   );
 }
 
