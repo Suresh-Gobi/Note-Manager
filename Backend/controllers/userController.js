07d563b394985689b2de853d5e740bf9e01ada16
Suresh_Gobi <sureshgobi34@gmail.com>
2023-09-03 16:31:30 +0530

    created delete controller function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,29 +1,33 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-    
-        // Check if user with the same email already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-          return res.status(400).json({ message: 'User with this email already exists' });
-        }
-    
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-    
-        // Create a new user with hashed password
-        const newUser = await User.create({ username, email, password: hashedPassword });
-    
-        // Note that we're saving the hashed password, not the original password
-        
-        res.status(201).json(newUser);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user with the same email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user with hashed password
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 exports.login = async (req, res) => {
@@ -33,20 +37,18 @@ exports.login = async (req, res) => {
     // Check if user with the provided email exists
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
-    // Compare the provided password with the hashed password
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
-    // Generate JWT token with email included in the payload
     const token = jwt.sign(
-      { userId: user._id, username: user.username, email: user.email }, // Include email in the payload
+      { userId: user._id, username: user.username, email: user.email }, 
       process.env.JWT_SECRET,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" }
     );
 
     res.status(200).json({ token });
@@ -55,17 +57,16 @@ exports.login = async (req, res) => {
   }
 };
 
-
 exports.addNote = async (req, res) => {
   try {
     const { noteTitle, noteSubject, note } = req.body;
-    const userId = req.user.userId; // From the JWT verification middleware
+    const userId = req.user.userId;
 
     // Find the user by userId
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Add the new note to the user's notes array
@@ -74,7 +75,7 @@ exports.addNote = async (req, res) => {
     // Save the updated user document
     await user.save();
 
-    res.status(201).json({ message: 'Note added successfully' });
+    res.status(201).json({ message: "Note added successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -82,18 +83,46 @@ exports.addNote = async (req, res) => {
 
 exports.getAllNotes = async (req, res) => {
   try {
-    const userId = req.user.userId; // From the JWT verification middleware
+    const userId = req.user.userId; 
 
-    // Find the user by userId
+    
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Return the user's notes
     res.status(200).json({ notes: user.notes });
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteNote = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const noteId = req.params.noteId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+
+    const noteIndex = user.notes.findById(
+      (note) => note._id.toString() === noteId
+    );
+    if (nodeIndex === -1) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    user.notes.splice(noteIndex, 1);
+
+    await user.save();
+
+    res.status(200).json({ message: "Note Deleted" });
+  } catch (error)
+  {
     res.status(500).json({ message: error.message });
   }
 };
